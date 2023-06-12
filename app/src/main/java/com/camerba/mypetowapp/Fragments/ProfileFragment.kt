1f1d5b2d121e17fba4ed1f07bada64612bd019db
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-13 00:45:38 +0300

    initial commit

--- app/src/main/java/com/camerba/mypetowapp/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/camerba/mypetowapp/Fragments/ProfileFragment.kt
@@ -10,16 +10,12 @@ import android.widget.Button
 import com.camerba.mypetowapp.AccountSettingsActivity
 import com.camerba.mypetowapp.R
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ProfileFragment : Fragment() {
 
     private var param1: String? = null
