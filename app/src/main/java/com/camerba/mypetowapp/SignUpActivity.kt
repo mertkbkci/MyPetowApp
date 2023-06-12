203f06750f59185b4fe963f1adb971fdab47c417
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-13 01:55:00 +0300

    initial commit

--- app/src/main/java/com/camerba/mypetowapp/SignUpActivity.kt
+++ app/src/main/java/com/camerba/mypetowapp/SignUpActivity.kt
@@ -1,11 +1,17 @@
 package com.camerba.mypetowapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        findViewById<Button>(R.id.signin_link_btn).setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
