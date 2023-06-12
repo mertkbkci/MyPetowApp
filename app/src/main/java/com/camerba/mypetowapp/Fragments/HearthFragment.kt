751c14eb5688070eb65e8ea2638b7c115a9d49fe
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-12 22:26:03 +0300

    initial commit

--- app/src/main/java/com/camerba/mypetowapp/Fragments/HearthFragment.kt
+++ app/src/main/java/com/camerba/mypetowapp/Fragments/HearthFragment.kt
@@ -35,7 +35,7 @@ class HearthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hearth, container, false)
+        return inflater.inflate(R.layout.fragment_heart, container, false)
     }
 
     companion object {
