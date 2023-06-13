4bbd91b32281d7b03a90151fcb6eecb7cff7adf1
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-14 01:42:43 +0300

    initial commit

--- app/src/main/java/com/camerba/mypetowapp/Fragments/SearchFragment.kt
+++ app/src/main/java/com/camerba/mypetowapp/Fragments/SearchFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.camerba.mypetowapp.Adapter.UserAdapter
+import com.camerba.mypetowapp.Model.User
 import com.camerba.mypetowapp.R
+
+import kotlin.collections.ArrayList
 
 
 private const val ARG_PARAM1 = "param1"
@@ -13,23 +19,27 @@ private const val ARG_PARAM2 = "param2"
 
 
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var recyclerView : RecyclerView? = null
+    private var userAdapter: UserAdapter? = null
+    private var mUser : MutableList<User>? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_search, container, false)
+       val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_view_search)
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+
+        mUser = ArrayList()
+        userAdapter = context?.let { UserAdapter(it,mUser as ArrayList<User>,true) }
+        recyclerView?.adapter = userAdapter
+
+
+
+        return view
     }
 
 
