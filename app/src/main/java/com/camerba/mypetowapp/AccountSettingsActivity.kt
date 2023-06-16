effca7d5f2934dc438739981154de708cccd7846
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-16 13:06:20 +0300

    initial commit

--- app/src/main/java/com/camerba/mypetowapp/AccountSettingsActivity.kt
+++ app/src/main/java/com/camerba/mypetowapp/AccountSettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.camerba.mypetowapp
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -8,22 +10,29 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.camerba.mypetowapp.Model.User
-import com.canhub.cropper.CropImageContract
-import com.canhub.cropper.CropImageView
-import com.canhub.cropper.options
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import de.hdodenhof.circleimageview.CircleImageView
 import java.util.Locale
+import kotlin.coroutines.Continuation
 
 class AccountSettingsActivity : AppCompatActivity() {
     private lateinit var firebaseUser: FirebaseUser
     private var checker = ""
+    private var myUrl= ""
+    private var imageUri : Uri? = null
+    private var storageProfilePicRef: StorageReference? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +40,7 @@ class AccountSettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_account_settings)
 
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
+          storageProfilePicRef =FirebaseStorage.getInstance().reference.child("Profile Pictures")
 
         findViewById<Button>(R.id.logout_btn).setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -41,18 +51,39 @@ class AccountSettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        findViewById<TextView>(R.id.change_image_text_btn).setOnClickListener {
+            checker = "clicked"
+            CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(this@AccountSettingsActivity)
+        }
 
         findViewById<Button>(R.id.save_info_profile_btn).setOnClickListener {
             if (checker == "clicked") {
 
+                uploadImageAndUpdateInfo()
 
 
-            } else {
+            }
+            else {
                 updateUserInfoOnly()
             }
         }
 
         userInfo()
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null){
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            findViewById<CircleImageView>(R.id.profile_image_view_profile_frag).setImageURI(imageUri)
+        }
+
+
     }
 
     private fun updateUserInfoOnly() {
@@ -107,4 +138,35 @@ class AccountSettingsActivity : AppCompatActivity() {
             }
         })
     }
+    private fun uploadImageAndUpdateInfo() {
+
+        when{
+            imageUri == null -> Toast.makeText(this, "Lütfen önce fotğrafınızı seçin.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(findViewById<TextView>(R.id.full_name_profile_frag).text.toString()) ->
+                Toast.makeText(this, "Lütfen önce tam adınızı yazın.", Toast.LENGTH_LONG).show()
+            (findViewById<TextView>(R.id.usermane_profile_frag).text.toString()) == "" ->
+                Toast.makeText(this, "Lütfen önce kullanıcı adınızı yazın.", Toast.LENGTH_LONG).show()
+            (findViewById<TextView>(R.id.bio_profile_frag).text.toString()) == " " ->
+                Toast.makeText(this, "Lütfen önce bionuzu yazın.", Toast.LENGTH_LONG).show()
+
+            else ->{
+                val fileRef = storageProfilePicRef!!.child(firebaseUser!!.uid + "jpg")
+                var uploadTask:StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+                uploadTask.continueWithTask (com.google.android.gms.tasks.Continuation <UploadTask.TaskSnapshot,Task<Uri>>{ task ->
+
+                    if (!task.isSuccessful){
+                        task.exception?.let {
+                            throw it
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                })
+
+
+            }
+        }
+
+    }
+
 }
